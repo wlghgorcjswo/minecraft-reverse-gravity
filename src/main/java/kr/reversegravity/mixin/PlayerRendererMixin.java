a952699ac9dca1ca16cba8883bc7df0c8b538efe
wlghgorcjswo <126782824+wlghgorcjswo@users.noreply.github.com>
2026-09-23 21:56:32 +0900

    Make renderer placeholder mapping-safe

--- src/main/java/kr/reversegravity/mixin/PlayerRendererMixin.java
+++ src/main/java/kr/reversegravity/mixin/PlayerRendererMixin.java
@@ -1,9 +1,6 @@
 package kr.reversegravity.mixin;
 
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PlayerRenderer.class)
-public abstract class PlayerRendererMixin {
-    // Player-model inversion will be refined after the 26.3 mappings compile check.
+// Reserved for upside-down player model rendering after 26.3 renderer mappings are verified.
+public final class PlayerRendererMixin {
+    private PlayerRendererMixin() {}
 }
