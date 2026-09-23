f01e324a47adbb56909e8ba7dde5f2be50848f30
wlghgorcjswo <126782824+wlghgorcjswo@users.noreply.github.com>
2026-09-23 21:54:24 +0900

    Add src/main/java/kr/reversegravity/ReverseGravityMod.java

---
+++ src/main/java/kr/reversegravity/ReverseGravityMod.java
@@ -0,0 +1,10 @@
+package kr.reversegravity;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class ReverseGravityMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Core behavior is applied by mixins.
+    }
+}
